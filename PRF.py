3cdee1a2383a1622340525c9ba487757e9aebc36
Zhao Haishan <hyaloids@163.com>
2018-06-12 20:28:36 +0800

    version 1.0

---
+++ PRF.py
@@ -0,0 +1,60 @@
+#实现查准率、召回率与F1的计算。
+def tf_confusion_metrics(model, actual_clases, session, feed_dict):
+    predictions = tf.argmax(model, 1)
+    actuals = tf.argmax(actual_clases, 1)
+
+    ones_like_actuals = tf.ones_like(actuals)
+    zeros_like_actuals = tf.zeros_like(actuals)
+    ones_like_predictions = tf.ones_like(predictions)
+    zeros_like_predictions = tf.zeros_like(predictions)
+
+    tp_op = tf.reduce_sum(
+        tf.cast(
+            tf.logical_and(
+                tf.equal(actuals, ones_like_actuals),
+                tf.equal(predictions, ones_like_predictions)
+            ),
+            "float"
+        )
+    )
+
+    tn_op = tf.reduce_sum(
+        tf.cast(
+            tf.logical_and(
+                tf.equal(actuals,zeros_like_actuals),
+                tf.equal(predictions, zeros_like_predictions)
+            ),
+            "float"
+        )
+    )
+
+    fn_op = tf.reduce_sum(
+        tf.cast(
+            tf.logical_and(
+                tf.equal(actuals, ones_like_actuals),
+                tf.equal(predictions, zeros_like_predictions)
+            ),
+            "float"
+        )
+    )
+
+    tp, tn, fp, fn = \
+    session.run(
+        [tp_op, tn_op, fp_op, fn_op],
+        feed_dict
+    )
+
+    tpr = float(tp)/(float(tp)+float(fn))
+    fpr = float(fp)/(float(tp)+float(fn))
+
+    accuracy = (float(tp)+float(tn))/(float(tp)+float(fp)+float(fn)+float(tn))
+
+    recall = tpr
+    precision = float(tp)/(float(tn)+float(fp))
+
+    f1_score = (2 * (precision * recall)) / (precision + recall)
+
+    print('Precision = ',precision)
+    print('Recall = ',recall)
+    print('F1_score = ',f1_score)
+    print('Accuracy = ', accuracy)
